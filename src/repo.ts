d3ef13b756404df567c0a7e1ea78a2e177c471bd
Emanuel Unge <emanuel.unge@digiexam.se>
2023-08-10 20:20:13 +0200

    update firebase to 10

--- src/repo.ts
+++ src/repo.ts
@@ -3,56 +3,56 @@ import {
   collectionGroup,
   doc as docRef,
   getDoc,
-  type Firestore, type Query, type DocumentReference, type CollectionReference
+  type Firestore, type Query, type DocumentReference, type CollectionReference, type DocumentData
 } from 'firebase/firestore'
 import { type CollectionRepo, type SubCollection, type SubCollectionRepo } from './types'
 
-function getDocRef <T> (collection: CollectionReference<T>, id: string): DocumentReference<T> {
-  return docRef<T>(collection, id)
+function getDocRef <T, D extends DocumentData> (collection: CollectionReference<T, D>, id: string): DocumentReference<T, D> {
+  return docRef<T, D>(collection, id)
 }
 
-function makeDocRef <T> (collection: CollectionReference<T>): DocumentReference<T> {
-  return docRef<T>(collection)
+function makeDocRef <T, D extends DocumentData> (collection: CollectionReference<T, D>): DocumentReference<T, D> {
+  return docRef<T, D>(collection)
 }
 
 const fieldNameFactory = <T>() => (name: keyof T) => name
 
-export const createCollection = <T>(firestore: Firestore, collectionName: string): CollectionReference<T> => {
-  return collection(firestore, collectionName) as CollectionReference<T>
+export const createCollection = <T, D extends DocumentData>(firestore: Firestore, collectionName: string): CollectionReference<T, D> => {
+  return collection(firestore, collectionName) as CollectionReference<T, D>
 }
-export const createSubCollection = <TSub>(collectionName: string):
-SubCollection<TSub> => (doc: DocumentReference):
-  CollectionReference<TSub> => collection(doc, collectionName) as CollectionReference<TSub>
+export const createSubCollection = <TSub, D extends DocumentData>(collectionName: string):
+SubCollection<TSub, D> => (doc: DocumentReference):
+  CollectionReference<TSub, D> => collection(doc, collectionName) as CollectionReference<TSub, D>
 
-export function createCollectionRepo<T> (firestore: Firestore, collectionName: string): CollectionRepo<T> {
-  const collectionRef = createCollection<T>(firestore, collectionName)
-  return createRepoFromRef<T>(collectionRef)
+export function createCollectionRepo<T, D extends DocumentData> (firestore: Firestore, collectionName: string): CollectionRepo<T, D> {
+  const collectionRef = createCollection<T, D>(firestore, collectionName)
+  return createRepoFromRef<T, D>(collectionRef)
 }
 
-export function createSubCollectionRepo<TSub> (firestore: Firestore, parent: string, collectionName: string): SubCollectionRepo<TSub> {
+export function createSubCollectionRepo<TSub, D extends DocumentData> (firestore: Firestore, parent: string, collectionName: string): SubCollectionRepo<TSub, D> {
   const parentCollection = collection(firestore, parent)
-  const subCollection = createSubCollection<TSub>(collectionName)
-  return createSubCollectionRepoFromRef<TSub>(subCollection, parentCollection, collectionName)
+  const subCollection = createSubCollection<TSub, D>(collectionName)
+  return createSubCollectionRepoFromRef<TSub, D>(subCollection, parentCollection, collectionName)
 }
 
-export function createRepoFromRef<T> (col: CollectionReference<T>): CollectionRepo<T> {
+export function createRepoFromRef<T, D extends DocumentData> (col: CollectionReference<T, D>): CollectionRepo<T, D> {
   return {
     fieldName: fieldNameFactory<T>(),
     collection: col,
-    docRef: (id: string) => getDocRef<T>(col, id),
-    doc: async (id: string) => await getDoc<T>(getDocRef<T>(col, id)),
-    new: () => makeDocRef<T>(col)
+    docRef: (id: string) => getDocRef<T, D>(col, id),
+    doc: async (id: string) => await getDoc<T, D>(getDocRef<T, D>(col, id)),
+    new: () => makeDocRef<T, D>(col)
   }
 }
 
-export function createSubCollectionRepoFromRef<TSub> (col: SubCollection<TSub>, parent: CollectionReference, name: string): SubCollectionRepo<TSub> {
+export function createSubCollectionRepoFromRef<TSub, D extends DocumentData> (col: SubCollection<TSub, D>, parent: CollectionReference, name: string): SubCollectionRepo<TSub, D> {
   return {
     collection: col,
     fieldName: fieldNameFactory<TSub>(),
     collectionById: (parentId: string) => col(getDocRef(parent, parentId)),
     docRef: (parentId: string, id: string) => getDocRef(col(getDocRef(parent, parentId)), id),
-    doc: async (parentId: string, id: string) => await getDoc<TSub>(getDocRef(col(getDocRef(parent, parentId)), id)),
-    new: (parentId: string) => makeDocRef<TSub>(col(getDocRef(parent, parentId))),
-    collectionGroup: collectionGroup(parent.firestore, name) as Query<TSub>
+    doc: async (parentId: string, id: string) => await getDoc<TSub, D>(getDocRef(col(getDocRef(parent, parentId)), id)),
+    new: (parentId: string) => makeDocRef<TSub, D>(col(getDocRef(parent, parentId))),
+    collectionGroup: collectionGroup(parent.firestore, name) as Query<TSub, D>
   }
 }
